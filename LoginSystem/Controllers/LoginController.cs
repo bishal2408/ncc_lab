9e59c9a4652182b5a97c1f78e231fcb632723a21
your pc <email@example.com>
2022-07-13 12:55:02 +0545

    Login System NCC

---
+++ LoginSystem/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginSystem.Models;
+
+namespace LoginSystem.Controllers
+{
+    public class LoginController : Controller
+    {
+        db_loginEntities db = new db_loginEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(login l)
+        {
+            if (ModelState.IsValid == true)
+            {
+                var data = db.logins.Where(x=>x.username == l.username && x.password == l.password).FirstOrDefault();
+                if (data == null)
+                {
+                    ViewBag.Message = "error username and password";
+                    return View();
+                }
+                else
+                {
+                    Session["username"] = l.username;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View();
+        }
+    }
+}
